f5c5b7c5aff12d2f9603f747527616066edb8a01
KimMinyoung <st92035@gmail.com>
2021-02-08 14:33:12 +0900

    Initial commit

---
+++ src/test/java/com/kim/myhome/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
